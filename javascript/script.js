fa09cd0f9145fcdc3fbac54d21e2815e4a3fd6ec
Desta <destakawestara66@gmail.com>
2024-10-24 14:26:55 +0700

    next

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-
+// Contact us validate form
 function validateForm() {
     let inputUsername = document.getElementById('nama').value;
     let inputTanggal = document.getElementById('tanggal').value;
@@ -6,68 +6,69 @@ function validateForm() {
     let inputPesan = document.getElementById('pesan').value;
 
     if (inputUsername == '') {
-        alert('Input kosong!');
+        alert('Input Nama Kosong!');
     }
     else {
-        document.getElementById('text-output').value;
+        // document.getElementById('text-output').value = document.getElementById('nama').value;
         console.log(inputUsername);
     }
 
     if (inputTanggal == '') {
-        alert('Input kosong!');
+        alert('Input Tanggal Kosong!');
     }
     else {
-        document.getElementById('text-output').value;
+        // document.getElementById('text-output').value = document.getElementById('tanggal').value;
         console.log(inputTanggal);
     }
 
     if (inputGender == '') {
-        alert('Input kosong!');
+        alert('Input Gender Kosong!');
     }
     else {
-        document.getElementById('text-output').value;
+        // document.getElementById('text-output').value = document.getElementById('gender').value;
         console.log(inputGender);
     }
 
     if (inputPesan == '') {
-        alert('Input kosong!');
+        alert('Input Pesan Kosong!');
     }
     else {
-        document.getElementById('text-output').value;
+        // document.getElementById('text-output').value = document.getElementById('pesan').value;
         console.log(inputPesan);
     }
 
-    let indexSlide = 0;
-    nextSlide(1);
+    // document.getElementById('text-output').value = document.getElementById('nama', 'tanggal', 'gender', 'pesan').value;
+    document.getElementById('text-output').value = print('nama', 'tanggal', 'gender', 'pesan').value;
 
-    function nextSlide(n) {
-        showSlide(indexSlide += n);
-    }
-    function showSlide() {
-        let listImage = document.getElementsByClassName("banner");
+}
 
-        if (n > listImage.length - 1){
-            indexSlide = 0;
-        }
+// Auto/Manual Banner Slider
+let indexSlide = 0;
+nextSlide(1);
 
-        if (n < 0) {
-            indexSlide = listImage.length - 1;
-        }
-        
+function nextSlide(n) {
+    showSlide(indexSlide += n);
+}
+function showSlide() {
+    let listImage = document.getElementsByClassName("banner");
 
-        let index = 0;
-        while (index < listImage.length) {
-            listImage[index].computedStyleMap.display = 'none';
-            index++;
-        }
-
-        listImage[indexSlide].computedStyleMap.display = 'block';
-        console.log(listImage);
-        console.log(n);
+    if (n > listImage.length - 1) {
+        indexSlide = 0;
     }
 
-    console.log("Hello World")
+    if (n < 0) {
+        indexSlide = listImage.length - 1;
+    }
 
+    let index = 0;
+    while (index < listImage.length) {
+        listImage[index].getElementsByClassName.display = 'none';
+        index++;
+    }
+
+    listImage[indexSlide].getElementsByClassName.display = 'block';
+    console.log(listImage);
+    console.log(n);
 }
 
 // setInterval(() == nextSlide(1), 2000);
